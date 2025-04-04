632d2f4bfa4589b8582f69c6c8b1991fa249f4b4
jacksmithinsulander <666.jack.smith@protonmail.com>
2025-04-04 21:52:37 +0800

    added some boilerplate

--- main.py
+++ main.py
@@ -3,5 +3,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/ping")
-def read_root():
+def ping():
+    # Health check, easily test that api is live
     return "pong"
+
+@app.get("/feed")
+def get_feed()
+
+@app.get("/following")
+def get_following()
+
+@app.get("/followers")
+def get_followers()
+
+@app.get("/communities")
+def get_communities
+

92c1dee44d118072ad7a0e7f618e3a3703408c6d
larsyngvelundin <nok13@live.se>
2025-04-04 17:01:10 +0200

    importing Profile

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
 from fastapi import FastAPI
+from v1.processors.profile import Profile
 
 app = FastAPI()
+
 
 @app.get("/ping")
 def ping():
     # Health check, easily test that api is live
     return "pong"
 
+
 @app.get("/feed/{wallet}")
 def get_feed():
     pass
+
 
 @app.get("/following/{wallet}")
 def get_following():
     pass
 
+
 @app.get("/followers/{wallet}")
 def get_followers():
     pass
+
 
 @app.get("/profile/{wallet}")
 def get_profile():
     pass
 
+
 @app.post("/signUp/{wallet}")
 def sign_up():
-    pass 
+    pass
+
 
 @app.get("/communities")
 def get_communities():
     pass
 
+
 @app.post("/addContract")
 def add_contract():
     pass
 
+
 @app.post("/interactWithContract")
 def interact_with_contract():
     pass
-
-

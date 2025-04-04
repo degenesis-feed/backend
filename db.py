324a91d287b8965e5f396f85c6558c518999d6dc
larsyngvelundin <nok13@live.se>
2025-04-04 16:46:50 +0200

    get_followers function added

--- db.py
+++ db.py
@@ -16,6 +16,20 @@ def get_followings(con, address: str):
         return clean_results
 
 
+def get_followers(con, address: str):
+    query = """
+        SELECT user_address FROM followings WHERE following_address = ?;
+    """
+    with con:
+        cursor = con.cursor()
+        cursor.execute(query, (address,))
+        results = cursor.fetchall()
+        clean_results = []
+        for result in results:
+            clean_results.append(result[0])
+        return clean_results
+
+
 def add_following(con, user_address: str, following_address: str):
     query = """
         INSERT INTO followings(user_address, following_address) VALUES

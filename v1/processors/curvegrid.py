8efc4b613caef34966a29e0c45cc757168ae6d15
larsyngvelundin <nok13@live.se>
2025-04-05 18:01:33 +0200

    cache abis and check for cached abi

--- v1/processors/curvegrid.py
+++ v1/processors/curvegrid.py
@@ -2,24 +2,35 @@ import os
 import requests
 from dotenv import load_dotenv
 from v1.utils.feedme_status import FeedMeStatus
+from db.db import get_abi, add_abi
+from db.db_setup import get_connection
 
 load_dotenv()
 
-CGURL = os.getenv("CURVEGRID_URL") 
+CGURL = os.getenv("CURVEGRID_URL")
 CGJWT = os.getenv("CURVEGRID_JWT")
 CHAIN = "ethereum"
 
+
 def make_contract_instance(contract_address: str) -> FeedMeStatus:
+    con = get_connection()
+    cached_abi = get_abi(con, contract_address)
+    if cached_abi:
+        return FeedMeStatus.SUCCESS.create("Address info: ", cached_abi)
+
     query = {"include": "contractLookup"}
     final_url = f"{CGURL}/api/v0/chains/{CHAIN}/addresses/{contract_address}"
 
     header = {"Authorization": f"Bearer {CGJWT}"}
 
     res = requests.get(final_url, params=query, headers=header)
-
+    add_abi(con, contract_address, res.text)
     return FeedMeStatus.SUCCESS.create("Address info: ", res.text)
 
-def interact_with_contract_method(wallet: str, contract_address: str, method: str, args: list[str] | None) -> FeedMeStatus:
+
+def interact_with_contract_method(
+    wallet: str, contract_address: str, method: str, args: list[str] | None
+) -> FeedMeStatus:
     final_url = f"{CGURL}/api/v0/chains/{CHAIN}/addresses/{wallet}/contracts/{contract_address}/methods/{method}"
 
     if args:
@@ -28,4 +39,4 @@ def interact_with_contract_method(wallet: str, contract_address: str, method: st
     else:
         res = requests.post(final_url)
 
-    return FeedMeStatus.SUCCESS.create("Interaction response: ", res.text);
+    return FeedMeStatus.SUCCESS.create("Interaction response: ", res.text)

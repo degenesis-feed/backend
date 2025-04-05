aa7f9b1937ac8d09bd17aed8a76c56550b091f80
larsyngvelundin <nok13@live.se>
2025-04-05 03:41:46 +0200

    first version of get historical

--- nodit.py
+++ nodit.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import os
 import requests
 from dotenv import load_dotenv
@@ -15,12 +16,46 @@ if not webhook_url:
 
 class Nodit:
     def __init__(self, api_key):
+        self.api_key = api_key
         pass
 
     def _get_api_url(self, network: str, action: str) -> str:
+        match action:
+            case "historical_transfers":
+                url = f"https://web3.nodit.io/v1/{network}/mainnet/token/getTokenTransfersByAccount"
+                return url
+        # url = "https://web3.nodit.io/v1/arbitrum/mainnet/token/getTokenTransfersByAccount"
+
         pass
 
-    def get_historical(self, address: str) -> list:
+    def get_historical(
+        self, address: str, from_date: str = "", to_date: str = ""
+    ) -> list:
+        """
+        Get historical token transfers for a wallet
+        If no from_date and to_date is specified, defaults to last 30 days
+        """
+        url = self._get_api_url(network="ethereum", action="historical_transfers")
+        if not from_date or not to_date:
+            current_time = datetime.now()
+            to_date = current_time.isoformat()
+            thirty_days_before = current_time - timedelta(days=30)
+            from_date = thirty_days_before.isoformat()
+        payload = {
+            "accountAddress": address,
+            "fromDate": from_date,
+            "toDate": to_date,
+            "withCount": False,
+            "withZeroValue": False,
+        }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "X-API-KEY": self.api_key,
+        }
+        response = requests.post(url, json=payload, headers=headers)
+
+        print(response.text)
         pass
 
     def add_webhook(self, addresses):
@@ -34,22 +69,21 @@ class Nodit:
 
 
 # def get_api_url()
+# n = Nodit(api_key=api_key)
+# n.get_historical("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045")
 
-api_url = "https://web3.nodit.io/v1/ethereum/mainnet/webhooks"
+# payload = {
+#     "eventType": "SUCCESSFUL_TRANSACTION",
+#     "description": "Webhook for successful transaction",
+#     "notification": {"webhookUrl": f"{webhook_url}"},
+#     "condition": {"addresses": ["0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97"]},
+# }
+# headers = {
+#     "accept": "application/json",
+#     "content-type": "application/json",
+#     "X-API-KEY": api_key,
+# }
 
+# response = requests.post(url, json=payload, headers=headers)
 
-payload = {
-    "eventType": "SUCCESSFUL_TRANSACTION",
-    "description": "Webhook for successful transaction",
-    "notification": {"webhookUrl": f"{webhook_url}"},
-    "condition": {"addresses": ["0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97"]},
-}
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "X-API-KEY": api_key,
-}
-
-response = requests.post(url, json=payload, headers=headers)
-
-print(response.text)
+# print(response.text)
